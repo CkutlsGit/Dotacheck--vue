0aa1e414565799873b3da3622849a97d2164c0b8
Kostya <ckutls228@gmail.com>
2023-11-12 15:23:38 +0300

    fix: error with CORS

--- dotacheck/api/getHero.js
+++ dotacheck/api/getHero.js
@@ -1,33 +1,29 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-export default function() {
-    return {
-        getHeroCunterPicks(hero) {
-            return new Promise((resolve, reject) => {
-                const url = `https://ru.dotabuff.com/heroes/${hero}`
+export function getHeroCunterPicks(hero) {
+    return new Promise((resolve, reject) => {
+        const url = `https://ru.dotabuff.com/heroes/${hero}`;
 
-                axios.get(url)
-                    .then((response) => {
-                        if (response.status === 200) {
-                            const $ = cheerio.load(response.data)
-                            const heroNames = []
+        axios.get(url)
+            .then((response) => {
+                if (response.status === 200) {
+                    const $ = cheerio.load(response.data);
+                    const heroNames = [];
 
-                            $('.link-type-hero').each((index, element) => {
-                                const heroName = $(element).text();
-                                heroNames.push(heroName)
-                            })
-
-                            const heroCounterPick = heroNames.slice(-10)
-                            resolve(heroCounterPick);
-                        } else {
-                            reject(new Error('Failed to fetch data'));
-                        }
-                    })
-                    .catch(error => {
-                        reject(error);
+                    $('.link-type-hero').each((index, element) => {
+                        const heroName = $(element).text();
+                        heroNames.push(heroName);
                     });
+
+                    const heroCounterPick = heroNames.slice(-10);
+                    resolve(heroCounterPick);
+                } else {
+                    reject(new Error('Failed to fetch data'));
+                }
+            })
+            .catch(error => {
+                reject(error);
             });
-        }
-    }
+    });
 }
