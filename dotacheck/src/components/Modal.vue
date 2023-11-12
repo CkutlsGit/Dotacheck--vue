38c0030ba6ac4df6b958f265e9037baea5074ab7
Kostya <ckutls228@gmail.com>
2023-11-12 14:59:59 +0300

    feat: add logic for check, valid name hero or not

--- dotacheck/src/components/Modal.vue
+++ dotacheck/src/components/Modal.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="modal">
     <input type="text" placeholder="name hero" v-model.trim="hero">
-    <button type="submit" @click="sendData">Submit</button>
+    <button type="submit" @click="sendData" :disabled="isHeroValid">Submit</button>
   </div>
 </template>
 
 <script>
+import heroList from "@/mocks/HeroList";
+
 export default {
   name: 'Modal-WriteHero',
   data() {
     return {
       hero: ''
+    }
+  },
+  computed: {
+    isHeroValid() {
+      return !heroList.heroNames.map(name => name.toLowerCase()).includes(this.hero.toLowerCase())
     }
   },
   methods: {
