ad661eb813b537d299b7dfa816bf220aea215360
Kostya <ckutls228@gmail.com>
2023-11-12 14:48:07 +0300

    feat: add func for send data and add template

--- dotacheck/src/components/Modal.vue
+++ dotacheck/src/components/Modal.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="modal"></div>
+  <div class="modal">
+    <input type="text" placeholder="name hero" v-model.trim="hero">
+    <button type="submit" @click="sendData">Submit</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Modal-WriteHero'
+  name: 'Modal-WriteHero',
+  data() {
+    return {
+      hero: ''
+    }
+  },
+  methods: {
+    sendData() {
+      this.$emit('sendData', this.hero)
+    }
+  }
 }
 </script>
