b7d1f58e9ae0243ba8ac36727a771753de1dcd81
Kostya <ckutls228@gmail.com>
2023-11-12 14:53:06 +0300

    fix: change name hero in hero list

--- dotacheck/src/mocks/HeroList.js
+++ dotacheck/src/mocks/HeroList.js
@@ -9,7 +9,7 @@ export default {
         'Grimstroke', 'Gyrocopter', 'Hoodwink', 'Huskar', 'Invoker', 'Io', 'Jakiro', 'Juggernaut',
         'Keeper of the Light', 'Kunkka', 'Legion Commander', 'Leshrac', 'Lich', 'Lifestealer',
         'Lina', 'Lion', 'Lone Druid', 'Luna', 'Lycan', 'Magnus', 'Marci', 'Mars', 'Medusa',
-        'Meepo', 'Mirana', 'Monkey King', 'Morphling', 'Muerta', 'Naga Siren', "Nature's Prophet",
+        'Meepo', 'Mirana', 'Monkey King', 'Morphling', 'Muerta', 'Naga Siren', "Natures Prophet",
         'Necrophos', 'Night Stalker', 'Nyx Assassin', 'Ogre Magi', 'Omniknight', 'Oracle',
         'Outworld Destroyer', 'Pangolier', 'Phantom Assassin', 'Phantom Lancer', 'Phoenix',
         'Primal Beast', 'Puck', 'Pudge', 'Pugna', 'Queen of Pain', 'Razor', 'Riki', 'Rubick',
