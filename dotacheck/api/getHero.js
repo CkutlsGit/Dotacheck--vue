a90404ff96900d8ab5e58831e4e658e401435f18
Kostya <ckutls228@gmail.com>
2023-11-12 14:34:50 +0300

    feat: add axios request

--- dotacheck/api/getHero.js
+++ dotacheck/api/getHero.js
@@ -1,1 +1,33 @@
-export default
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+export default function(instance) {
+    return {
+        getHeroCunterPicks(hero) {
+            return new Promise((resolve, reject) => {
+                const url = `https://ru.dotabuff.com/heroes/${hero}`
+
+                axios.get(url)
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const $ = cheerio.load(response.data)
+                            const heroNames = []
+
+                            $('.link-type-hero').each((index, element) => {
+                                const heroName = $(element).text();
+                                heroNames.push(heroName)
+                            })
+
+                            const heroCounterPick = heroNames.slice(-10)
+                            resolve(heroCounterPick);
+                        } else {
+                            reject(new Error('Failed to fetch data'));
+                        }
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        }
+    }
+}
