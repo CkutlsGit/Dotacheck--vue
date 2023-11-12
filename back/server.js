dc95f80e4bef28f17b485cb3238d9c707c6823e4
Kostya <ckutls228@gmail.com>
2023-11-12 15:30:29 +0300

    feat: add server logic

--- back/server.js
+++ back/server.js
@@ -1,3 +1,38 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/heroes/:hero', async(req, res) => {
+    const { hero } = req.params
+    try {
+        const url = 'https://ru.dotabuff.com/heroes/${hero}'
+        const response = await axios.get(url)
+
+        if (response.status === 200) {
+            const $ = cheerio.load(response.data)
+            const heroNames = []
+
+            $('.link-type-hero').each((index, element) => {
+                const heroName = $(element).text()
+                heroNames.push(heroName)
+            })
+
+            const heroCounterPick = heroNames.slice(-10)
+            res.json(heroCounterPick)
+        }
+        else {
+            res.status(500).json({ error: 'Failed to fetch data' })
+        }
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Server run on port 3000')
+})
